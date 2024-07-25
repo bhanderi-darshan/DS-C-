dac538b28e144e513bf01b93d6e06bab4429e3f1
bhanderi-darshan <141249366+bhanderi-darshan@users.noreply.github.com>
2024-07-25 14:40:40 +0530

    Add files via upload

---
+++ +_0.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+int a;
+cout<<"Enter the number";
+cin>>a;
+if(a>0){
+    cout<<"The number is positive";
+}
+else if(a<0){
+    cout<<"The number is negative";
+
+}
+else if(a==0){
+    cout<<"The number is zero";
+}}
